12b71beb50239a6239d81041adf11a07d775f48a
wenshui012 <xiew2614@gmail.com>
2026-09-15 03:07:24 +0800

    release: prepare v4.5.1

--- src/ui-main.js
+++ src/ui-main.js
@@ -53,7 +53,7 @@
     var IMG_QUALITY = 0.8;
     var FAB_ID = 'tm-fab-main';
 
-    var TM_VERSION = options.version || '4.5.0';
+    var TM_VERSION = options.version || '4.5.1';
     var storageApi = null;
     var imageToolsApi = null;
     var imageLoaderApi = null;
